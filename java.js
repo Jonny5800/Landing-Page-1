1c99a357fd8fee6bd6955a59796db0b7f697d068
Jonny <jonny.hall5800@gmail.com>
2022-03-10 16:02:52 +0000

    Routine save

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
-alert('test')
+//alert('test')
+
+//function hover(element) {
+//    element.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+//}
+
+//function unhover(element) {
+//    element.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+//}
